5e93c4534a6485131cc6fea814ce644e392c1cd3
Kobioss <155322966+Kobioss@users.noreply.github.com>
2024-10-15 18:25:15 +0100

    script.js

--- docs/script.js
+++ docs/script.js
@@ -70,6 +70,20 @@ function myFunction() {
   }
 }
 
+// Get the input field
+var input = document.getElementById("password");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("Button1").click();
+  }
+});
+
 let popupAlreadyShowed = false
 document.getElementById('popup').style.display = 'block'
 popupAlreadyShowed = true
